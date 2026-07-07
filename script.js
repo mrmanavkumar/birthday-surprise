097812dc17be45aa7a78940232b392868d6086a3
mrmanavkumar <mrmanavkumar869@gmail.com>
2026-07-07 22:41:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,46 +1,15 @@
 function openGift() {
 
-    const gift = document.querySelector(".gift-box");
+document.querySelector(".gift-container").style.display="none";
 
-    gift.classList.add("shake");
+document.getElementById("surprise").style.display="block";
 
-    setTimeout(function () {
+document.getElementById("music").play();
 
-        // Gift Box hide
-        document.querySelector(".gift-container").style.display = "none";
-
-        // Surprise show
-        document.getElementById("surprise").style.display = "block";
-
-        // Music play
-        document.getElementById("music").play();
-
-        // Confetti
-        var duration = 10000;
-        var animationEnd = Date.now() + duration;
-
-        (function frame() {
-
-            confetti({
-                particleCount: 6,
-                angle: 60,
-                spread: 80,
-                origin: { x: 0 }
-            });
-
-            confetti({
-                particleCount: 6,
-                angle: 120,
-                spread: 80,
-                origin: { x: 1 }
-            });
-
-            if (Date.now() < animationEnd) {
-                requestAnimationFrame(frame);
-            }
-
-        })();
-
-    }, 800);
+confetti({
+particleCount:200,
+spread:120,
+origin:{y:0.6}
+});
 
 }
