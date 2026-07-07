4f9322f7d3bbf1cd31bb324a229deb56e327f9b5
mrmanavkumar <mrmanavkumar869@gmail.com>
2026-07-07 22:36:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,35 +6,40 @@ function openGift() {
 
     setTimeout(function () {
 
+        // Gift Box hide
         document.querySelector(".gift-container").style.display = "none";
+
+        // Surprise show
         document.getElementById("surprise").style.display = "block";
 
-        
+        // Music play
+        document.getElementById("music").play();
 
-var duration = 10000;
-var animationEnd = Date.now() + duration;
+        // Confetti
+        var duration = 10000;
+        var animationEnd = Date.now() + duration;
 
-(function frame() {
+        (function frame() {
 
-    confetti({
-        particleCount: 6,
-        angle: 60,
-        spread: 80,
-        origin: { x: 0 }
-    });
+            confetti({
+                particleCount: 6,
+                angle: 60,
+                spread: 80,
+                origin: { x: 0 }
+            });
 
-    confetti({
-        particleCount: 6,
-        angle: 120,
-        spread: 80,
-        origin: { x: 1 }
-    });
+            confetti({
+                particleCount: 6,
+                angle: 120,
+                spread: 80,
+                origin: { x: 1 }
+            });
 
-    if (Date.now() < animationEnd) {
-        requestAnimationFrame(frame);
-    }
+            if (Date.now() < animationEnd) {
+                requestAnimationFrame(frame);
+            }
 
-})();
+        })();
 
     }, 800);
 
