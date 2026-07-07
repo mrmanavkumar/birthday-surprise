1b4c19cc4c466403ff54117984e27ff7c4b19dd1
mrmanavkumar <mrmanavkumar869@gmail.com>
2026-07-07 22:54:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,38 @@
 function openGift() {
 
+    // Gift box shake
     document.querySelector(".gift-box").classList.add("shake");
 
     setTimeout(function () {
 
+        // Gift hide
         document.querySelector(".gift-container").style.display = "none";
 
+        // Surprise show
         document.getElementById("surprise").style.display = "block";
 
+        // Music play
         const music = document.getElementById("music");
-
         music.currentTime = 0;
         music.play();
 
-        var duration = 10000;
+        // Confetti for 45 seconds
+        var duration = 45000;
         var animationEnd = Date.now() + duration;
 
         (function frame() {
 
             confetti({
-                particleCount: 6,
+                particleCount: 8,
                 angle: 60,
-                spread: 80,
+                spread: 90,
                 origin: { x: 0 }
             });
 
             confetti({
-                particleCount: 6,
+                particleCount: 8,
                 angle: 120,
-                spread: 80,
+                spread: 90,
                 origin: { x: 1 }
             });
 
