043401bd5995ee5475f735277430c37d4550fe97
mrmanavkumar <mrmanavkumar869@gmail.com>
2026-07-10 19:52:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -17,10 +17,7 @@ function openGift() {
         countdown.play().catch(err => {
             console.log("Countdown play failed:", err);
         });
-        const music = document.getElementById("music");
-music.currentTime = 0;
-music.play().catch(err => console.log(err));
-music.pause();
+        
         document.getElementById("countdownScreen").style.display = "flex";
 
 const countdownText = document.getElementById("countdownText");
